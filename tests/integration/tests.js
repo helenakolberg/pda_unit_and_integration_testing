e68c6747393b3361132f583e01ec3afbb0177e2c
Helena <inger.helena@gmail.com>
2020-09-25 16:34:30 +0100

    should be able to chain multiple operations pass

--- tests/integration/tests.js
+++ tests/integration/tests.js
@@ -31,5 +31,15 @@ describe('calculator functionality', function() {
     expect(running_total.getAttribute('value')).to.eventually.equal('28');
   });
 
+  it('should be able to chain multiple operations', function() {
+    element(by.css('#number8')).click();
+    element(by.css('#operator_subtract')).click();
+    element(by.css('#number4')).click();
+    element(by.css('#operator_add')).click();
+    element(by.css('#number2')).click();
+    element(by.css('#operator_equals')).click();
+    expect(running_total.getAttribute('value')).to.eventually.equal('6');
+  })
+
 
 });
